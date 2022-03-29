ca93cf1471f841bab5e7658794a69ad0e59d3ffd
oyc0401 <oyc0401@gmail.com>
2022-03-29 14:50:30 +0900

    기능 구현 끝

--- lib/page/edit.dart
+++ lib/page/edit.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -17,11 +20,9 @@ class EditPage extends StatefulWidget {
 }
 
 class _EditPageState extends State<EditPage> {
-
   late userInfo userinfo = widget.userinfo;
-
-  List<userInfo> list=[];
-
+  List answers = ['', '', '', '', '', '', ''];
+  int numline = 5;
 
   @override
   void initState() {
@@ -30,14 +31,9 @@ class _EditPageState extends State<EditPage> {
     init();
   }
 
-  init() async{
-    SQLite sqLite=await SQLite.Instance();
-    list= await sqLite.getInfo();
-  }
-
-  save(userInfo user) async{
-    SQLite sqLite=await SQLite.Instance();
-    await sqLite.insertTime(user);
+  init() async {
+    answers = userinfo.answerList();
+    numline = answers.length;
   }
 
   @override
@@ -48,32 +44,148 @@ class _EditPageState extends State<EditPage> {
       ),
       body: ListView(
         children: [
-          TextFormField(
-            initialValue: widget.userinfo.title,
-            decoration: const InputDecoration(
-              labelText: 'title',
-            ),
-            onChanged: (value) {
-              userinfo.title=value;
-            },
-          ),
-          TextFormField(
-            initialValue: widget.userinfo.grade,
-            decoration: const InputDecoration(
-              labelText: 'grade',
-            ),
-            onChanged: (value) {
-              userinfo.grade=value;
-            },
-          ),
-          
+          CupertinoButton(
+              child: Text('줄 추가하기 (현재 $numline 줄)'), onPressed: addLine),
+          inputSection(),
           CupertinoButton(
               child: Text('저장하기'),
               onPressed: () {
-                save(userinfo);
+                save(userinfo, context);
               })
         ],
       ),
     );
+  }
+
+  String String2Sha256(String text) {
+    var bytes = utf8.encode(text); // data being hashed
+    var digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  lastRemove(List list) {
+    for (int i = 1; i <= list.length; i++) {
+      if (list.last == '') {
+        list.removeLast();
+      } else {
+        break;
+      }
+    }
+    return list;
+  }
+
+  save(userInfo user, BuildContext context) async {
+    print(answers);
+
+    List list = lastRemove(answers);
+    userinfo.answers = ListToString(list);
+
+    print(userinfo.toMap());
+
+    SQLite sqLite = await SQLite.Instance();
+    await sqLite.insertTime(user);
+
+    Navigator.of(context).pop(true);
+  }
+
+  ListToString(List list) {
+    String string = '';
+
+    list.forEach((element) {
+      string = '$string,$element';
+    });
+    print(string);
+    return string;
+  }
+
+  addLine() {
+    setState(() {
+      numline = numline + 5;
+      for (int i = 1; i <= 5; i++) {
+        answers.add('');
+      }
+    });
+  }
+
+  Widget inputSection() {
+    List<Widget> list = [];
+    for (int i = 1; i <= numline; i++) {
+      list.add(inputWidget(i));
+    }
+
+    Widget widget = Column(
+      children: [
+        TextFormField(
+          initialValue: userinfo.title,
+          textInputAction: TextInputAction.next,
+          decoration: const InputDecoration(
+            labelText: 'title',
+          ),
+          onChanged: (value) {
+            userinfo.title = value;
+          },
+        ),
+        TextFormField(
+          initialValue: userinfo.grade,
+          textInputAction: TextInputAction.next,
+          decoration: const InputDecoration(
+            labelText: 'grade',
+          ),
+          onChanged: (value) {
+            userinfo.grade = value;
+          },
+        ),
+        ...list
+      ],
+    );
+    return widget;
+  }
+
+  Widget inputWidget(int num) {
+    Color color = Colors.white;
+    if (num % 10 > 5) {
+      color = Colors.black12;
+    } else if (num % 10 == 0) {
+      color = Colors.black12;
+    }
+
+    Widget widget = Padding(
+        padding: EdgeInsets.all(8),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+          decoration: BoxDecoration(
+              color: color,
+              border: Border.all(
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(300))),
+          child: Row(
+            children: [
+              Text(
+                '$num번: ',
+                style: TextStyle(fontSize: 18),
+              ),
+              Expanded(
+                  child: TextFormField(
+                initialValue: answers[num - 1],
+                style: TextStyle(fontSize: 16),
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  answers[num - 1] = value;
+                  print(answers);
+                  if (numline == num) {
+                    print('줄추가해해해해');
+                    addLine();
+                  }
+                },
+              ))
+            ],
+          ),
+        ));
+
+    return widget;
   }
 }
